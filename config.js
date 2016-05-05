dbbe528beaa459ded0a10964a4811e24babf8c80
Bill Romanowich <willroman@hotmail.com>
2016-05-05 08:57:03 -0400

    Refer to the breeze javascript package using its name on npm: breeze-client

--- config.js
+++ config.js
@@ -24,7 +24,8 @@ System.config({
     "aurelia-tools": "npm:aurelia-tools@0.1.22",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
-    "breeze": "github:breeze/breeze.js@1.5.6",
+    "breeze": "breeze-client",
+    "breeze-client": "npm:breeze-client@1.5.6",
     "core-js": "npm:core-js@1.2.6",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
